eed20b96abbe6e7d53ef0d1970e3fa887b703fb8
mindfulme <ceo@citvy.com>
2020-09-09 20:35:58 +0300

    deployed

---
+++ precache-manifest.dbdea114789bd122f461142762e0c8eb.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d276b82d00d86c9e8eed098a8af2be1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e531b33f9837a1c3418a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e42e1e7ada7ee346b457",
+    "url": "/static/js/2.72529419.chunk.js"
+  },
+  {
+    "revision": "4a027f1c5e76cb932a8683d567e5a1b5",
+    "url": "/static/js/2.72529419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e531b33f9837a1c3418a",
+    "url": "/static/js/main.eec37a1d.chunk.js"
+  },
+  {
+    "revision": "57066941ba93488d08ed",
+    "url": "/static/js/runtime-main.655418d0.js"
+  },
+  {
+    "revision": "b24396917a138794160b24ff04a71d80",
+    "url": "/static/media/WorkSans-VariableFont_wght.b2439691.ttf"
+  },
+  {
+    "revision": "e881b3eccc8add2401ff9f847ec9eb47",
+    "url": "/static/media/aBAT-logo.e881b3ec.png"
+  },
+  {
+    "revision": "712e0d7f11f5622ccbabe336c7fbce7d",
+    "url": "/static/media/aBUSD-logo.712e0d7f.png"
+  },
+  {
+    "revision": "7d2fd5333d6cc4013a3bb06ec7704789",
+    "url": "/static/media/aDAI-logo.7d2fd533.png"
+  },
+  {
+    "revision": "c24821a43ea5dea7399a75785d4364c5",
+    "url": "/static/media/aETH-logo.c24821a4.png"
+  },
+  {
+    "revision": "6938a4300bcda73ed135133224de258c",
+    "url": "/static/media/aKNC-logo.6938a430.png"
+  },
+  {
+    "revision": "1cc319ec2349b478c0405f863ebc4ebe",
+    "url": "/static/media/aLEND-logo.1cc319ec.png"
+  },
+  {
+    "revision": "1c00e7c46413fe8b24f9966a645167dc",
+    "url": "/static/media/aLINK-logo.1c00e7c4.png"
+  },
+  {
+    "revision": "b091957fa78e53f98fdc0b1971c8e780",
+    "url": "/static/media/aMANA-logo.b091957f.png"
+  },
+  {
+    "revision": "374fe2394bf6ecec4c570be6ede2f689",
+    "url": "/static/media/aMKR-logo.374fe239.png"
+  },
+  {
+    "revision": "7ba733790f20df8a57c4f8be663b956e",
+    "url": "/static/media/aREP-logo.7ba73379.png"
+  },
+  {
+    "revision": "c9de216bc91456f619c61806b5465d46",
+    "url": "/static/media/aSNX-logo.c9de216b.png"
+  },
+  {
+    "revision": "9c874e737af30c3564d69a900cad68c8",
+    "url": "/static/media/aSUSD-logo.9c874e73.png"
+  },
+  {
+    "revision": "803236550813c5c5e72f17ac0530cd91",
+    "url": "/static/media/aTUSD-logo.80323655.png"
+  },
+  {
+    "revision": "99d3189e3b4f318d83c5582436da6c21",
+    "url": "/static/media/aUSDC-logo.99d3189e.png"
+  },
+  {
+    "revision": "a251fdec4a2035ad7cafe19b412345e8",
+    "url": "/static/media/aUSDT-logo.a251fdec.png"
+  },
+  {
+    "revision": "72d1e651b5356ff7eae680c5e3d161e0",
+    "url": "/static/media/aWBTC-logo.72d1e651.png"
+  },
+  {
+    "revision": "ecb77c8725752bc5417f919c4ae5b966",
+    "url": "/static/media/aZRX-logo.ecb77c87.png"
+  },
+  {
+    "revision": "bebcbda3160bf4065af3979fc80ae1a9",
+    "url": "/static/media/aave-logo.bebcbda3.png"
+  },
+  {
+    "revision": "62825e60a8dc908aef6082461f302133",
+    "url": "/static/media/icn-ledger.62825e60.svg"
+  },
+  {
+    "revision": "e8109252c38f5f7874506c6731c52292",
+    "url": "/static/media/icn-metamask.e8109252.svg"
+  },
+  {
+    "revision": "edcc1ab5dde5cb3d5cf134c4aade641b",
+    "url": "/static/media/trustWallet.edcc1ab5.png"
+  }
+]);
